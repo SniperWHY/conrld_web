68f918d60266899b5b209761dff0c96fda033620
WHY <why@MacBook-Air.local>
2019-09-04 18:07:08 +0800

    register page

---
+++ src/components/Slider.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/slider.scss';
+
+export default (props) => {
+	return (
+		<div className='track'>
+			<div className='slider' />
+		</div>
+	)
+}
--- src/views/Register.js
+++ src/views/Register.js
@@ -2,13 +2,36 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import * as Action from "../action";
 import { connect } from 'react-redux';
+import {
+	Steps
+} from 'antd';
+import Slider from "../components/Slider";
 
 import '../styles/register.scss';
 class Register extends React.Component {
 	render() {
 		return (
 			<div className='register'>
-				
+				<div className='nav'>
+					<div className='logo'>
+						LOGO 预留
+					</div>
+					<div className='title'>
+						创建Conrld ID
+					</div>
+					<Slider className="track"/>
+				</div>
+				<div className='register_content'>
+					<h1>填写您的Email</h1>
+					<Steps>
+						<Steps.Step>
+						</Steps.Step>
+						<Steps.Step>
+						</Steps.Step>
+						<Steps.Step>
+						</Steps.Step>
+					</Steps>
+				</div>
 			</div>
 		)
 	}
